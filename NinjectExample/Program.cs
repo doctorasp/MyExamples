f5debd24eb848aa9173e9f3bd25b0e51891a64d8
doctorasp <deepbrain2077@gmail.com>
2016-10-15 22:39:37 +0300

    ninject

---
+++ NinjectExample/Program.cs
@@ -0,0 +1,69 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectExample
+{
+    class Program
+    {
+        public static IKernel AppKernel;
+
+        public class Bazuka : IWeapon
+        {
+            public void Kill()
+            {
+                Console.WriteLine("Bazuka");
+            }
+        }
+
+        public class Sword : IWeapon
+        {
+            public void Kill()
+            {
+                Console.WriteLine("Sword");
+            }
+        }
+
+        public class Warrior
+        {
+            readonly IWeapon _weapon;
+
+            public Warrior(IWeapon weapon)
+            {
+                this._weapon = weapon;
+            }
+
+            public void Kill()
+            {
+                _weapon.Kill();
+            }
+        }
+
+        public class AnotherWarrior
+        {
+            [Inject]
+            public IWeapon Weapon { get; set; }
+
+            public void Kill()
+            {
+                Weapon.Kill();
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            AppKernel = new StandardKernel(new WeaponNinjectModule());
+            var warrior = AppKernel.Get<Warrior>();
+            warrior.Kill();
+
+            var anotherWarrior = AppKernel.Get<AnotherWarrior>();
+            anotherWarrior.Kill();
+            Console.ReadLine();
+        }
+    }
+}
